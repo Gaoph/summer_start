6434a221e99de990f9ab289dbd227b994a5f1ae3
Gaoph <414917512@qq.com>
2016-10-31 15:25:30 +0800

    文章

---
+++ MyBlog/src/main/java/com/myblog/dao/ArticleMapper.java
@@ -0,0 +1,17 @@
+package com.myblog.dao;
+
+import com.myblog.entity.Article;
+
+public interface ArticleMapper {
+    int deleteByPrimaryKey(Integer articleId);
+
+    int insert(Article record);
+
+    int insertSelective(Article record);
+
+    Article selectByPrimaryKey(Integer articleId);
+
+    int updateByPrimaryKeySelective(Article record);
+
+    int updateByPrimaryKey(Article record);
+}
